6d992e9f6d8d724ac56f0de1490367a53cc7cbe1
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-18 21:29:39 +0000

    Updated variable names

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@ import { ref, onMounted } from 'vue';
 import UserTestimonial from './components/UserTestimonial.vue';
 
 const _isLoading = ref(true)
-const _data = ref([]);
+const _testimonialData = ref([]);
 
 const fetchData = async () => {
   try {
     const response = await fetch("./assets/db/data.json");
     const jsonData = await response.json();
-    _data.value = jsonData;
+    _testimonialData.value = jsonData;
     _isLoading.value = false;
   } catch (error) {
     console.error("Error loading data", error)
@@ -21,9 +21,9 @@ onMounted(() => fetchData())
 </script>
 
 <template>
-  <h1 v-if="_isLoading">Loading testimonials...</h1>
+  <p v-if="_isLoading">Loading testimonials...</p>
   <main v-else class="w-10/12 grid grid-cols-1">
-    <UserTestimonial v-for="{id,user_name, avatar_url,user_status,feedback_title, feedback_text} in _data"
+    <UserTestimonial v-for="{id,user_name, avatar_url,user_status,feedback_title,feedback_text} in _testimonialData"
       :user_name="user_name" :user_status="user_status" :avatar_url="avatar_url" :feedback_title="feedback_title"
       :feedback_text="feedback_text" :key="id" />
 
